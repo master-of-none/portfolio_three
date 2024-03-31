2bbc85046ac45163dc0ab5cca0dfc1722a609a88
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-03-31 11:24:10 -0700

    Added various 3D models

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,26 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Loader from "../components/Loader";
+import Island from "../models/Island";
+import Sky from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
 
 const Home = () => {
+    const adjustIslandForScreenSize = () => {
+        let screenScale = null;
+        let screenPosition = [0, -6.5, -43];
+        let rotation = [0.1, 4.7, 0];
+        if (window.innerWidth < 768) {
+            screenScale = [0.9, 0.9, 0.9];
+        } else {
+            screenScale = [1, 1, 1];
+        }
+        return [screenScale, screenPosition, rotation];
+    };
+
+    const [islandScale, islandPosition, islandRotation] =
+        adjustIslandForScreenSize();
     return (
         <section className="w-full h-screen relative">
             {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -12,7 +30,23 @@ const Home = () => {
                 className="w-full h-screen bg-transparent"
                 camera={{ near: 0.1, far: 1000 }}
             >
-                <Suspense fallback={<Loader />}></Suspense>
+                <Suspense fallback={<Loader />}>
+                    <directionalLight position={[1, 1, 1]} intensity={2} />
+                    <ambientLight intensity={0.5} />
+                    <hemisphereLight
+                        skyColor="#b1e1ff"
+                        groundColor="#000000"
+                        intensity={1}
+                    />
+                    <Bird />
+                    <Sky />
+                    <Island
+                        position={islandPosition}
+                        scale={islandScale}
+                        rotation={islandRotation}
+                    />
+                    <Plane />
+                </Suspense>
             </Canvas>
         </section>
     );
